93cd360c596cc2bf0b0eae6ff23dffbb1071530f
ChamudiAyashmi <chamudi2003@gmail.com>
2023-09-23 16:37:42 +0530

    add js

--- Front End/js/app.js
+++ Front End/js/app.js
@@ -10,3 +10,14 @@ const today = new Date().getDay();
 const dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturaday"];
 const currentDay =document.getElementById("day");
 currentDay.textContent = dayList[today];
+
+const searchIcon = document.getElementById("secrch-icon");
+const cityName = document.getElementById("cityName");
+
+searchIcon.addEventListener("click",()=>{
+  const searchbarValue = document.getElementById("searchbar-text");
+  cityName.innerText = searchbarValue.value;
+})
+
+
+
