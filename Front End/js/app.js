b5d7204e3b82afdf304da36f811d587dd11f19be
ChamudiAyashmi <chamudi2003@gmail.com>
2023-09-23 22:33:48 +0530

    adding map

--- Front End/js/app.js
+++ Front End/js/app.js
@@ -11,13 +11,68 @@ const dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sa
 const currentDay =document.getElementById("day");
 currentDay.textContent = dayList[today];
 
-const searchIcon = document.getElementById("secrch-icon");
-const cityName = document.getElementById("cityName");
 
-searchIcon.addEventListener("click",()=>{
-  const searchbarValue = document.getElementById("searchbar-text");
-  cityName.innerText = searchbarValue.value;
-})
+// function findCity(){
+//   const searchIcon = document.getElementById("secrch-icon");
+//   const cityName = document.getElementById("cityName");
+//   const searchbarValue = document.getElementById("searchbar-text");
+//   cityName.innerText = searchbarValue.value;
+// }
 
+function findCity(){
+  const searchbar = $("#searchbar-text");
+  var typedText = searchbar.val();
+  const cityName = $("#cityName");
+  cityName.text(typedText);
+}
+
+
+
+// $(function () {
+//   var bindDatePicker = function() {
+//    $(".date").datetimepicker({
+//        format:'YYYY-MM-DD',
+//      icons: {
+//        time: "fa fa-clock-o",
+//        date: "fa fa-calendar",
+//        up: "fa fa-arrow-up",
+//        down: "fa fa-arrow-down"
+//      }
+//    }).find('input:first').on("blur",function () {
+//      // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+//      // update the format if it's yyyy-mm-dd
+//      var date = parseDate($(this).val());
+
+//      if (! isValidDate(date)) {
+//        //create date based on momentjs (we have that)
+//        date = moment().format('YYYY-MM-DD');
+//      }
+
+//      $(this).val(date);
+//    });
+//  }
+  
+//   var isValidDate = function(value, format) {
+//    format = format || false;
+//    // lets parse the date to the best of our knowledge
+//    if (format) {
+//      value = parseDate(value);
+//    }
+
+//    var timestamp = Date.parse(value);
+
+//    return isNaN(timestamp) == false;
+//   }
+  
+//   var parseDate = function(value) {
+//    var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+//    if (m)
+//      value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+
+//    return value;
+//   }
+  
+//   bindDatePicker();
+// });
 
 
