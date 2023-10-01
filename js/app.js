8937fbf720416459e3ad343ca63fdc8aee818462
ChamudiAyashmi <chamudi2003@gmail.com>
2023-10-01 16:52:57 +0530

    Update Dark Theme

--- js/app.js
+++ js/app.js
@@ -454,7 +454,7 @@ let btnDark = document.getElementById("btnDark");
 btnDark.addEventListener("click",()=>{
   if(modeSelector%2==0){
     document.body.style.backgroundColor = "#D9D9D9";
-    themeChangeImg.style.backgroundImage="url(https://fpdl.in/uploads/20231001081028_[fpdl.in]_sunrise-rural-area-with-pathway_116220-82_large.jpg)";
+    themeChangeImg.style.backgroundImage="url('../assets/morning.jpg')";
     divtodayHighlights.style.backgroundColor = "#FFFFFF";
     divForecast.style.backgroundColor = "#FFFFFF";
     iconLocation.style.color = "#000";
@@ -569,7 +569,7 @@ btnDark.addEventListener("click",()=>{
 
   }else{
     document.body.style.backgroundColor = "#2B2D42";
-    themeChangeImg.style.backgroundImage="url(https://fpdl.in/uploads/20231001081815_[fpdl.in]_night-ocean-landscape-full-moon-stars-shine_107791-7397_medium.jpg)";
+    themeChangeImg.style.backgroundImage="url('../assets/night.jpg')";
     divtodayHighlights.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
     iconLocation.style.color = "#FFFFFF";
     searchIcon.style.color = "#FFFFFF";
